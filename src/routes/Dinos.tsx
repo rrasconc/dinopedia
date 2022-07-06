3cb2c5e1f40655ce17c4bff6bee8561413e5b5d4
Rigoberto Rascon <rigobertorascon5@gmail.com>
2022-07-06 13:22:35 -0700

    home redesign

--- src/routes/Dinos.tsx
+++ src/routes/Dinos.tsx
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 import { dinosaur } from "../types";
+import imgUrl from "../assets/dino.png";
 
 export function Dinos() {
   const [homeDinos, setHomeDinos] = useState<dinosaur[]>([]);
@@ -15,7 +16,7 @@ export function Dinos() {
     const dinosAll = await response.json();
     setDinosAll(dinosAll);
 
-    const dinosRandom = dinosAll.sort(() => 0.5 - Math.random()).slice(0, 8);
+    const dinosRandom = dinosAll.sort(() => 0.5 - Math.random()).slice(0, 10);
 
     const dinoList = await Promise.all(
       dinosRandom.map(async (dino: string) => {
@@ -33,15 +34,21 @@ export function Dinos() {
 
   return (
     <div className="mb-12">
-      <section className="flex flex-col items-center justify-center">
-        <h1 className="text-7xl mt-32 text-slate-700 font-bold">dinopedia</h1>
-        <form className="flex items-center w-6/12 my-24 py-2 px-4 rounded-lg border-2 bg-zinc-50 focus:border-slate-400">
+      <section
+        style={{
+          height: "70vh",
+        }}
+        className="flex flex-col mb-24 items-center justify-center bg-slate-200"
+      >
+        <img className="h-36 mt-36" src={imgUrl} />
+        <h1 className="text-7xl  text-slate-700 font-bold">dinopedia</h1>
+        <form className="flex items-center w-11/12 md:w-8/12 my-24 py-2 px-4 rounded-lg border-2 border-slate-300 bg-zinc-100 hover:border-slate-700">
           <FontAwesomeIcon
             className="mr-4 text-slate-400"
             icon={faMagnifyingGlass}
           />
           <input
-            className="w-full text-xl text-slate-700 bg-zinc-50 focus:outline-none"
+            className="w-full text-xl text-slate-700 bg-zinc-100 focus:outline-none"
             type="text"
             placeholder={`Search for one of the ${dinosAll.length} dinosaurs`}
           />
@@ -49,12 +56,12 @@ export function Dinos() {
       </section>
       <section className="flex flex-wrap justify-center">
         {homeDinos.map((dino) => (
-          <div className="flex flex-col items-center hover:scale-105 transition delay-75 ease-out duration-200">
-            <h1 className=" bg-slate-200 rounded-md p-2 mb-2 w-96 text-center ">
+          <div className="flex flex-col items-center group hover:scale-105 transition delay-75 ease-out duration-200">
+            <h1 className=" bg-slate-200 rounded-md p-2 mb-2 w-72 text-center ">
               {dino.name}
             </h1>
             <Link
-              className=" border-2 rounded-md w-96 h-96 py-2 mx-4 mb-4"
+              className=" border-2 rounded-md w-72 h-72 py-2 mx-4 mb-4 group-hover:border-slate-700"
               key={dino.name}
               to={`dino/${dino}`}
             >
